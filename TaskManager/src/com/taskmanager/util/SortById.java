54aada08d1c2eac7f279ed9bde02ac93b2579a19
Marlon Braga <marlon.braga@ibm.com>
2025-03-10 02:10:54 -0500

    SortById class

---
+++ TaskManager/src/com/taskmanager/util/SortById.java
@@ -0,0 +1,16 @@
+package com.taskmanager.util;
+
+import com.taskmanager.Task;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortById implements TaskSorter {
+
+    @Override
+    public void sort(List<Task> tasks) {
+        Collections.sort(tasks, Comparator.comparing(Task::getId));
+    }
+
+}
