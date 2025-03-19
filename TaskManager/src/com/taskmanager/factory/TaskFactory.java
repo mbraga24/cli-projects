3e69ff189aba7194f63fefd2f91effa21af10599
Marlon Braga <marlon.braga@ibm.com>
2025-03-18 20:01:50 -0500

    remove id

--- TaskManager/src/com/taskmanager/factory/TaskFactory.java
+++ TaskManager/src/com/taskmanager/factory/TaskFactory.java
@@ -12,9 +12,9 @@ public class TaskFactory {
     public static Task createTask(TaskType type, int id, String title, String description, Date dueDate, String extraDetails) {
         switch(type) {
             case WORK:
-                return new WorkTask(id, title, description, dueDate, extraDetails);
+                return new WorkTask(title, description, dueDate, extraDetails);
             case PERSONAL:
-                return new PersonalTask(id, title, description, dueDate, extraDetails);
+                return new PersonalTask(title, description, dueDate, extraDetails);
             default:
                 throw new IllegalArgumentException("Invalid Task Type");
         }
