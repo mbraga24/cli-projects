94b98d08e4383f2ab0dfa157fa5e28b2482f4a07
Marlon Braga <marlon.braga@ibm.com>
2025-03-27 18:00:34 -0500

    TaskManager: preparing for testing

---
+++ TaskManager/test/domain/factory/TaskFactoryTest.java
@@ -0,0 +1,5 @@
+package domain.factory;
+
+//✅ Test that it creates correct WorkTask and PersonalTask instances with expected fields.
+public class TaskFactoryTest {
+}
---
+++ TaskManager/test/repository/TaskManagerDAO.java
@@ -0,0 +1,6 @@
+package repository;
+
+//If it’s in-memory: ✅ Write unit tests.
+//If it connects to a database: ⚠️ Write integration tests (using H2).
+public class TaskManagerDAO {
+}
