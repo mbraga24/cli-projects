d65a419f341f4a299464bdb9dd831ccdaa62a7ae
Marlon Braga <marlon.braga@ibm.com>
2025-04-04 15:05:38 -0500

    TaskManager: add proper dependency injection

--- TaskManager/src/main/java/Main.java
+++ TaskManager/src/main/java/Main.java
@@ -1,15 +1,19 @@
 import com.taskmanager.actions.Actions;
 import com.taskmanager.controller.TaskController;
+import com.taskmanager.repository.TaskDataAccessService;
 import com.taskmanager.repository.TaskFakerDataAccessService;
+import com.taskmanager.repository.TaskManagerDAO;
+import com.taskmanager.service.TaskManagerService;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        TaskFakerDataAccessService taskFakerDataAccessService = new TaskFakerDataAccessService();
+        TaskManagerDAO taskManagerDAO = new TaskDataAccessService();
+        TaskManagerService taskManagerService = new TaskManagerService(taskManagerDAO);
+        TaskFakerDataAccessService taskFakerDataAccessService = new TaskFakerDataAccessService(taskManagerService);
         Actions actions = new Actions();
 
         new TaskController(actions, taskFakerDataAccessService).start();
-
     }
 }
